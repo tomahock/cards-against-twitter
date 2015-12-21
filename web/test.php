f563fe377e8c9b435b708dde718b8ce5bf3650d3
Tomahock <eu@tomahock.com>
2015-12-21 21:51:19 +0000

    x

--- web/test.php
+++ web/test.php
@@ -26,11 +26,26 @@ echo '<pre>';
 
 $i = 0;
 do{
+   echo $white[$i]['text'] . PHP_EOL;
+   echo $black[$i]['text'] . PHP_EOL;
    $str = str_replace('_', $white[$i]['text'], $black[$i]['text'] );
    echo $str;
    $i++;
 } while( strlen($str) > 160);
 
+$i--;
+$blackUsed = $black[$i]['used']++;
+$whiteUsed = $white[$i]['used']++;
+
+$sql = $db->prepare('UPDATE black set used=? WHERE id=?');
+$sql->bindParam(1, $blackUsed);
+$sql->bindParam(2, $black[$i]['id']);
+$sql->execute();
+
+$sql = $db->prepare('UPDATE white set used=? WHERE id=?');
+$sql->bindParam(1, $whiteUsed);
+$sql->bindParam(2, $white[$i]['id']);
+$sql->execute();
 
 echo 'FINAL: ' . $str;
 //print_r($result);
