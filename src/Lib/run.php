270380ab38266a786f8c66a19b284a85637905ab
tomahock <eu@tomahock.com>
2016-04-14 09:53:30 -0400

    Applied fixes from StyleCI

--- src/Lib/run.php
+++ src/Lib/run.php
@@ -3,10 +3,9 @@
  * Created by PhpStorm.
  * User: tomahock
  * Date: 21/12/15
- * Time: 20:58
+ * Time: 20:58.
  */
-
-require_once  __DIR__ . '/../bootstrap.php';
+require_once __DIR__.'/../bootstrap.php';
 
 $db = new PDO(DSN);
 
@@ -14,21 +13,20 @@ $sql = $db->prepare('SELECT * FROM black ORDER BY used ASC');
 $sql->execute();
 $black = $sql->fetchAll();
 
-
 $sql = $db->prepare('SELECT * FROM white ORDER BY used ASC');
 $sql->execute();
 $white = $sql->fetchAll();
 
 $i = 0;
-do{
-   $str = str_replace('_', $white[$i]['text'], $black[$i]['text'] );
-   if(strlen($str) > 140){
-      $i++;
-   }
-} while(strlen($str) > 140);
+do {
+    $str = str_replace('_', $white[$i]['text'], $black[$i]['text']);
+    if (strlen($str) > 140) {
+        $i++;
+    }
+} while (strlen($str) > 140);
 
-$blackUsed = (int)$black[$i]['used'] + 1;
-$whiteUsed = (int)$white[$i]['used'] + 1;
+$blackUsed = (int) $black[$i]['used'] + 1;
+$whiteUsed = (int) $white[$i]['used'] + 1;
 
 $sql = $db->prepare('UPDATE black set used=? WHERE id=?');
 $sql->bindParam(1, $blackUsed);
@@ -40,22 +38,21 @@ $sql->bindParam(1, $whiteUsed);
 $sql->bindParam(2, $white[$i]['id']);
 $sql->execute();
 
-$settings = array(
-   'oauth_access_token' => TWITTER_OAUTH_ACCESS_TOKEN,
+$settings = [
+   'oauth_access_token'        => TWITTER_OAUTH_ACCESS_TOKEN,
    'oauth_access_token_secret' => TWITTER_OAUTH_ACCESS_TOKEN_SECRET,
-   'consumer_key' => TWITTER_OAUTH_CONSUMER_KEY,
-   'consumer_secret' => TWITTER_OAUTH_CONSUMER_SECRET
-);
+   'consumer_key'              => TWITTER_OAUTH_CONSUMER_KEY,
+   'consumer_secret'           => TWITTER_OAUTH_CONSUMER_SECRET,
+];
 
 $url = 'https://api.twitter.com/1.1/statuses/update.json';
 $requestMethod = 'POST';
 
-$postfields = array(
+$postfields = [
    'status' => $str,
-);
+];
 
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->buildOauth($url, $requestMethod)
    ->setPostfields($postfields)
    ->performRequest();
-
--- src/Lib/setup.php
+++ src/Lib/setup.php
@@ -3,26 +3,25 @@
  * Created by PhpStorm.
  * User: tomahock
  * Date: 21/12/15
- * Time: 20:48
+ * Time: 20:48.
  */
-
-require_once  __DIR__ . '/../bootstrap.php';
+require_once __DIR__.'/../bootstrap.php';
 
 $black = file_get_contents('../src/Assets/black.txt');
 $white = file_get_contents('../src/Assets/white.txt');
 
-$db = new PDO( DSN );
-$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+$db = new PDO(DSN);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
-foreach(preg_split("/((\r?\n)|(\r\n?))/", $black) as $line){
-   $sql = $db->prepare('INSERT INTO black(text) VALUES(?)');
+foreach (preg_split("/((\r?\n)|(\r\n?))/", $black) as $line) {
+    $sql = $db->prepare('INSERT INTO black(text) VALUES(?)');
 
-   $sql->bindParam(1, $line);
-   $sql->execute();
+    $sql->bindParam(1, $line);
+    $sql->execute();
 }
 
-foreach(preg_split("/((\r?\n)|(\r\n?))/", $white) as $line){
-   $sql = $db->prepare('INSERT INTO white(text) VALUES(?)');
-   $sql->bindParam(1, $line);
-   $sql->execute();
+foreach (preg_split("/((\r?\n)|(\r\n?))/", $white) as $line) {
+    $sql = $db->prepare('INSERT INTO white(text) VALUES(?)');
+    $sql->bindParam(1, $line);
+    $sql->execute();
 }
